51c13bfd4fc952f2ad78650c9c8b5f0ee890f9f0
sanoarul123 <sanoarul123@gmail.com>
2024-03-29 01:27:13 +0600

    keyboard color

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
+function setbackgroundcolorofkeyboard(elementid){
+    const element = document.getElementById(elementid);
+    element.classList.alphabets('bg-orange-400');
+}
+
+function getrandomalphabet(){
+
+    const alphabets ='abcdefghijklmnopqrst';
+    const alphaseparates = alphabets.split('');
+    
+    const randomnumber = Math.random()*25;
+    const index = Math.round(randomnumber);
+
+    const alphabetsindex = alphaseparates[index];
+    // console.log(alphabetsindex);
+
+    // const changealpha = document.getElementById('changechar');
+    // const p = document.createElement('p');
+    // p.innerText = alphabetsindex;
+  
+    
+
+    // const phidden = getElementById('hiddenkrbo');
+    // phidden.classList.add('hidden');
+
+    // changealpha.appendChild(p);
+
+    const currentalphatake = document.getElementById('currentalpha');
+    currentalphatake.innerText=alphabetsindex;
+
+    setbackgroundcolorofkeyboard(alphabetsindex);
+
+
+
+}
+
+
+function setbackgroundcolorofkeyboard(elementid){
+    console.log(elementid);
+    const element = document.getElementById(elementid);
+    element.classList.add('bg-orange-400');
+}
+
 document.getElementById('play-now-btn').addEventListener('click',function(){
 
     const screencall = document.getElementById('screen-page');
@@ -6,4 +49,7 @@ document.getElementById('play-now-btn').addEventListener('click',function(){
 
     const palyground = document.getElementById('play-ground-id');
     palyground.classList.remove('hidden');
+
+    getrandomalphabet();
+
 })
